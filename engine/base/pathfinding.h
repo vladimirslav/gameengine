e3955b49251d003d47c686534ca302f731231dfa
vladimirslav <vladimirslav@gmail.com>
2015-09-15 21:24:29 +0300

    Commenting pathfinding

--- engine/base/pathfinding.h
+++ engine/base/pathfinding.h
@@ -40,11 +40,28 @@ template <typename T> tile_list Astar(T*** field,
                                       int xDest,
                                       int yDest,
                                       bool include_destination,
-                                      int unit_w = 1)
+                                      int unit_w)
 {
     if (areValidCoordinates(xStart, yStart, w, h) == false)
     {
         return {}; //empty list
+    }
+
+    // check if unit can actually fit in destination
+    if (include_destination)
+    {
+        for (int newX = xDest + 1; newX < xDest + unit_w; newX++)
+        {
+            if (areValidCoordinates(newX, yDest, w, h) == false)
+            {
+                return{}; // empty list, one of the destination tiles is out of bounds
+            }
+
+            if (checkFunc(field[newX][yDest]) == false)
+            {
+                return{}; // empty list, one of the destination tiles is occupied
+            }
+        }
     }
 
     tile_list closed_set;
@@ -52,6 +69,7 @@ template <typename T> tile_list Astar(T*** field,
 
     std::vector<std::vector<TileInfo>> evaluatedCoords(w); // array of array
 
+    // initialize values, calculate initial predictions:
     for (size_t x = 0; x < w; x++)
     {
         evaluatedCoords[x] = std::vector<TileInfo>(h);
@@ -64,26 +82,24 @@ template <typename T> tile_list Astar(T*** field,
     }
 
     bool destinationReached = false;
-    // add starting tiles to open list
-    for (int i = xStart; i < xStart + unit_w; i++)
+    // add starting tile to open list
+    evaluatedCoords[xStart][yStart].G = 0;
+    evaluatedCoords[xStart][yStart].isStartingTile = true;
+    evaluatedCoords[xStart][yStart].isVisited = true;
+
+    if (areValidCoordinates(xStart, yStart, w, h))
     {
-
-        evaluatedCoords[i][yStart].G = 0;
-        evaluatedCoords[i][yStart].isStartingTile = true;
-        evaluatedCoords[i][yStart].isVisited = true;
-
-        if (areValidCoordinates(i, yStart, w, h))
-        {
-            open_set.push_back({ i, yStart });
-        }
+        open_set.push_back({ xStart, yStart });
     }
 
+    // while we have not reached destination
+    // and there are tiles that can still be evaluated
     while (open_set.empty() == false && destinationReached == false)
     {
         TileCoords currentTile;
-        int minF = w * h;
-        size_t tileNum;
-        // select lowest F
+        int minF = w * h; // minimum cost that is required to reach destination
+        size_t tileNum;   // tile index number
+        // select assumed lowest-cost path
         for (size_t i = 0; i < open_set.size(); i++)
         {
             int F = evaluatedCoords[xStart][yStart].G + evaluatedCoords[xStart][yStart].H;
@@ -94,76 +110,90 @@ template <typename T> tile_list Astar(T*** field,
                 minF = F;
             }
         }
+
+        // make an array of adjacent coordinates
         TileCoords adjacentCoordinates[] = { { currentTile.x - 1, currentTile.y }, 
                                                 { currentTile.x + 1, currentTile.y },
                                                 { currentTile.x, currentTile.y - 1 },
                                                 { currentTile.x, currentTile.y + 1 } };
 
+        // ... then go through it and check the new possible tiles
         for (int i = 0; i < sizeof(adjacentCoordinates) / sizeof(*adjacentCoordinates); i++)
         {
             if (areValidCoordinates(adjacentCoordinates[i].x, adjacentCoordinates[i].y, w, h))
             {
                 if (std::find(closed_set.begin(), closed_set.end(), adjacentCoordinates[i]) == closed_set.end())
                 {
+                    int adjX = adjacentCoordinates[i].x; // to make code look cleaner
+                    int adjY = adjacentCoordinates[i].y; 
+                    // if this tile has not been visited:
                     if (std::find(open_set.begin(), open_set.end(), adjacentCoordinates[i]) == open_set.end())
                     {
-                        if (adjacentCoordinates[i].x == xDest && adjacentCoordinates[i].y == yDest)
+                        // we found are destination
+                        if (adjX == xDest && adjY == yDest)
                         {
                             evaluatedCoords[xDest][yDest].comeFrom.x = currentTile.x;
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].comeFrom.y = currentTile.y;
+                            evaluatedCoords[adjX][adjY].comeFrom.y = currentTile.y;
                             destinationReached = true;
                             break;
                         }
 
+                        // see if found tiles are valid through the whole unit width
                         bool validTiles = true;
-                        for (int newX = adjacentCoordinates[i].x; newX < adjacentCoordinates[i].x + unit_w; newX++)
+                        for (int newX = adjX; newX < adjX + unit_w; newX++)
                         {
-                            if (areValidCoordinates(newX, adjacentCoordinates[i].y, w, h) == false)
+                            // coordinates are valid?
+                            if (areValidCoordinates(newX, adjY, w, h) == false)
                             {
                                 validTiles = false;
                                 break;
                             }
 
-                            if (checkFunc(field[newX][adjacentCoordinates[i].y]) == false)
+                            // field can be visited?
+                            if (checkFunc(field[newX][adjY]) == false)
                             {
                                 validTiles = false;
                                 break;
                             }
                         }
-
-                        if (validTiles) // if we have an unocupied field
+                        
+                        // if newfound tile is OK:
+                        // we have an unocupied field
+                        if (validTiles)
                         {
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].comeFrom.x = currentTile.x;
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].comeFrom.y = currentTile.y;
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].G = evaluatedCoords[currentTile.x][currentTile.y].G + 1;
+                            // say that we've came to evaluated coordinates from the current tile
+                            evaluatedCoords[adjX][adjY].comeFrom.x = currentTile.x;
+                            evaluatedCoords[adjX][adjY].comeFrom.y = currentTile.y;
+                            evaluatedCoords[adjX][adjY].G = evaluatedCoords[currentTile.x][currentTile.y].G + 1;
                             open_set.push_back(adjacentCoordinates[i]);
                         }
                     }
                     else
                     {
-                        if (evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].G > evaluatedCoords[currentTile.x][currentTile.y].G + 1)
+                        // if we visited this tile already
+                        if (evaluatedCoords[adjX][adjY].G > evaluatedCoords[currentTile.x][currentTile.y].G + 1)
                         {
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].comeFrom.x = currentTile.x;
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].comeFrom.y = currentTile.y;
-                            evaluatedCoords[adjacentCoordinates[i].x][adjacentCoordinates[i].y].G = evaluatedCoords[currentTile.x][currentTile.y].G + 1;
+                            // but the path would be shorter if we moved to this tile
+                            // from the other one
+                            evaluatedCoords[adjX][adjY].comeFrom.x = currentTile.x;
+                            evaluatedCoords[adjX][adjY].comeFrom.y = currentTile.y;
+                            evaluatedCoords[adjX][adjY].G = evaluatedCoords[currentTile.x][currentTile.y].G + 1;
                         }
                     }
-                    /*
-                    if (currentTile.x == xDest && currentTile.y == yDest)
-                    {
-                        destinationReached = true; //found our objective
-                    }
-                    */
                 }
             }
         }
 
+        // move the current tile to visited
         closed_set.push_back(currentTile);
+        // remove it from unvisited tiles
         open_set.erase(open_set.begin() + tileNum);
     }
 
     if (destinationReached)
     {
+        // let's make a path
+        // go backwards from destination
         tile_list path;
         if (include_destination)
         {
@@ -172,6 +202,7 @@ template <typename T> tile_list Astar(T*** field,
 
         TileCoords currentTile = evaluatedCoords[xDest][yDest].comeFrom;
 
+        // ... until we reach the starting tile
         while (currentTile.x != xStart || currentTile.y != yStart)
         {
             path.push_back(currentTile);
@@ -182,6 +213,7 @@ template <typename T> tile_list Astar(T*** field,
     }
     else
     {
+        // could not find a way - return nothing
         return {};
     }
 }
